0747e41957891b893edccd72297e983ff0ae0b00
Pavel Knyazev <poulix.nova@mail.ru>
2023-08-18 22:34:04 +0300

    Refactoring

--- do_stuff_fuzzer.cpp
+++ do_stuff_fuzzer.cpp
@@ -14,8 +14,10 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   if (data[1] != 'b') {
     return 0;
   }
+
   std::string str(reinterpret_cast<const char *>(data), size);
   DoStuff(str);  // Disregard the output.
+
   if (data[0] == 'a') {
     return 0;
   }
